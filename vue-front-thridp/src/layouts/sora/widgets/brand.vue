1419774d0838dc4b169ee49ac32cae44631ab32c
NyaNekoplus <1359643068@qq.com>
2021-06-21 10:11:43 +0800

    Push test

---
+++ vue-front-thridp/src/layouts/sora/widgets/brand.vue
@@ -0,0 +1,27 @@
+<template>
+  <a href="https://sora.vin">
+    <ruby>
+      <span class="sakuraso brand-pad"><span>悠</span></span>
+
+      <span class="no brand-pad">の</span>
+
+      <span class="shironeko brand-pad">空</span>
+
+      <rp></rp><rt class="chinese-font">遠　い　空　へ</rt><rp></rp>
+    </ruby>
+  </a>
+</template>
+
+<script>
+export default {
+  name: "brand",
+  data: () => ({}),
+}
+</script>
+
+<style scoped>
+.brand-pad{
+  padding-left: 5px;
+  padding-right: 5px;
+}
+</style>
